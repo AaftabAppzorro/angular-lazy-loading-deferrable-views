90fe0945c7dae2624731c2630056f24df43146fe
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-07-23 20:46:41 +0530

    Fully committed at once

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [WelcomeComponent],
 })
-export class AppComponent {
-  title = 'angular-lazy-loading-deferrable-views';
-}
+export class AppComponent {}
